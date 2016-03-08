4ca7777a4887172e6bf190790a935e3ca423893d
David Brown <david.brown@consat.se>
2016-03-08 07:47:22 +0100

    Issue #2 Build credentials in constructor
    Signed-off-by: David Brown <david.brown@consat.se>

--- hooks/jira/jira_auth.py
+++ hooks/jira/jira_auth.py
@@ -6,16 +6,14 @@ import requests
 class JiraLogin(JiraHookCommand):
     def __init__(self, session, username, password):
         self.session  = session
-        self.username = username
-        self.password = password
+        self.credentials = {
+            'username': username,
+            'password': password
+        }
         self.endpoint = '/rest/auth/1/session'
 
     def __call__(self, url):
-        credentials = {
-            'username': self.username,
-            'password': self.password
-        }
-        result = self.session.post(url + self.endpoint, json=credentials)
+        result = self.session.post(url + self.endpoint, json=self.credentials)
         print(result.status_code)
 
 class JiraLogout(JiraHookCommand):
@@ -26,4 +24,3 @@ class JiraLogout(JiraHookCommand):
     def __call__(self, url):
         result = self.session.delete(url + self.endpoint)
         print(result.status_code)
-        print(result.text)
